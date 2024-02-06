fabda3b8abe1e8eb21df437976500ee41fbb0980
Rizkiputraramadhan <rizkikuliit@gmail.com>
2024-02-06 22:22:39 +0700

    create layout penjual

--- resources/views/web/backend/layouts/penjual/index.blade.php
+++ resources/views/web/backend/layouts/penjual/index.blade.php
@@ -5,14 +5,114 @@
     @include('components.head')
 </head>
 
-<body>
-    @include('components.admin.asideAdmin')
-    @include('components.admin.headerAdmin')
-    <main class="flex p-10 pt-0 sm:ml-64">
-        <div class="w-full h-auto bg-tertiary rounded-3xl">
-
+<body class="bg-gray-100 ">
+    @include('components.header')
+    <section class="bg-gray-100 ">
+        <div class="bg-black">
+            <div
+                class="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:px-32 text-gray-900">
+            </div>
         </div>
-    </main>
+        <div class="w-5/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md bg-peach bg-white lg:-mt-30 ">
+            <div class="lg:p-6 sm:p-12 -50 lg:flex  text-gray-800 rounded-lg">
+                <div class="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row ">
+                    <img src="https://source.unsplash.com/75x75/?portrait" alt=""
+                        class="self-center mt-2  flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start bg-gray-500 border-gray-300">
+                </div>
+                <div class=" pt-4 space-x-4 p-3 ml-2">
+                    <h4 class="text-xl ml-4 font-bold ">Griya Dahar <a class="text-sm font-semibold ml-2"
+                            href=""><i class="bi bi-pencil-square"></i> Edit</a></h4>
+                    <div class="flex flex-wrap items-center mt-2 mb-1 space-x-2">
+                        <div class="flex ">
+                            <button type="button" title="Rate 1 stars" aria-label="Rate 1 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    class="w-6 h-6 text-yellow-500">
+                                    <path
+                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                    </path>
+                                </svg>
+                            </button>
+                            <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    class="w-6 h-6 text-yellow-500">
+                                    <path
+                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                    </path>
+                                </svg>
+                            </button>
+                            <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    class="w-6 h-6 text-yellow-500">
+                                    <path
+                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                    </path>
+                                </svg>
+                            </button>
+                            <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    class="w-6 h-6 text-yellow-500">
+                                    <path
+                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                    </path>
+                                </svg>
+                            </button>
+                            <button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                    class="w-6 h-6 text-yellow-500">
+                                    <path
+                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                    </path>
+                                </svg>
+                            </button>
+                        </div>
+                        <span class="text-gray-600">49</span>
+                    </div>
+                    <div class="mt-3 lg:flex">
+                        <h1 class="font-semibold mr-5"><i class="bi bi-chat-left-dots mr-2 font-extrabold"></i>50 Ulasan
+                            dari pengunjung</h1>
+                        <h1 class="font-semibold "><i class="bi bi-geo-alt mr-2 font-extrabold"></i>Jl.Jeruk No 3, Tegal
+                        </h1>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        @extends('components.navGrid')
+        @section('sectionPenjual')
+            <section>
+                <div class="w-full justify-center grid grid-cols-2 gap-4">
+                    <div class="py-6 bg-white shadow-lg rounded-md text-gray-900">
+                        <div
+                            class="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
+                            <h1 class="text-3xl font-semibold leadi text-center lg:text-left"><i
+                                    class="bi bi-emoji-expressionless-fill"></i> Menu <br> 6 </h1>
+                        </div>
+                    </div>
+                    <div class="py-6 bg-white shadow-lg rounded-md text-gray-900">
+                        <div
+                            class="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
+                            <h1 class="text-3xl font-semibold leadi text-center lg:text-left"><i
+                                    class="bi bi-chat-left-dots mr-2 font-extrabold"></i> Ulasan <br> 3</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="py-6 mt-4 bg-white shadow-lg rounded-md text-gray-900">
+                    <div
+                        class="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 lg:space-y-0 lg:flex-row lg:justify-between">
+                        <h1 class="text-3xl font-semibold leadi text-center lg:text-left">Toko anda belum memiliki menu.
+                            <br>
+                            Yuk tambahkan sekarang!</h1> <br>
+                            <button type="button"
+                                class="px-8 py-3 mx-auto  items-center justify-center font-semibold rounded bg-primary text-gray-100">Tambah Menu <i class="bi bi-plus-circle"></i></button>
+                    </div>
+                    
+                </div>
+
+            </section>
+        @endsection
+    </section>
 </body>
 
 </html>
